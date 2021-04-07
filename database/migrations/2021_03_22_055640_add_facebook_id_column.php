61264ce3a9a79735790728aab545ccc4ea18f5b0
ronypermadi <ronnypermadi1@gmail.com>
2021-04-07 09:53:50 +0700

    update user migration

--- database/migrations/2021_03_22_055640_add_facebook_id_column.php
+++ database/migrations/2021_03_22_055640_add_facebook_id_column.php
@@ -14,7 +14,7 @@ class AddFacebookIdColumn extends Migration
     public function up()
     {
         Schema::table('users', function ($table) {
-            $table->string('facebook_id')->nullable();
+            $table->string('facebook_id')->after('google_id')->nullable();
         });
     }
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -63,10 +63,10 @@
             <hr class="block w-full mb-4 border-0 border-t border-gray-300" />
             <div class="flex flex-wrap justify-center">
                 <div class="w-full sm:w-1/2 sm:pr-2 mb-3 sm:mb-0">
-                  <a href="" class="w-full bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-2 focus:outline-none focus:shadow-outline">Register with Facebook</a>
+                  <a href="{{ url('auth/facebook') }}" class="w-full bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-2 focus:outline-none focus:shadow-outline">Register with Facebook</a>
                 </div>
                 <div class="w-full sm:w-1/2 sm:pl-2">
-                  <a href="" class="w-full bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline">Register with Google</a>
+                  <a href="{{ url('auth/google') }}" class="w-full bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline">Register with Google</a>
                 </div>
             </div>
         </form>
